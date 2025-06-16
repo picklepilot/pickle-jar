28c1ce6de272a3054fb60a9638d2197ec36d7db0
picklepilot <kaczmar.justin@gmail.com>
2025-06-15 21:01:58 -0500

    refactor: update the preview config

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,7 @@ import type { Preview } from '@storybook/vue3-vite'
 import { setup } from '@storybook/vue3-vite'
 import ThemeConfigurator from '../src/utils/ThemeConfigurator'
 import '../src/style.css'
+import '../src/styles/theme.css'
 
 const preview: Preview = {
     parameters: {
@@ -11,7 +12,22 @@ const preview: Preview = {
                 date: /Date$/i,
             },
         },
+        layout: 'centered',
+        backgrounds: {
+            default: 'light',
+            options: {
+                light: { name: 'Light', value: '#ffffff' },
+                dark: { name: 'Dark', value: '#0c0a09' },
+            },
+        },
     },
+    decorators: [
+        (story, context) => {
+            const background = context.globals.backgrounds?.value
+            document.documentElement.className = background || 'light'
+            return story()
+        },
+    ],
 }
 // Use setup() to configure global Vue components, directives, plugins, etc.
 setup((app: any) => {
@@ -40,7 +56,8 @@ setup((app: any) => {
             uiWindowToolbarWindowTopMenu: 'bg-white',
             uiWindowToolbarWindowTopMenuTitle: 'text-zinc-900',
             uiWindowToolbarWindowCloseButton: '[&>svg]:size-4 text-red-50',
-            uiWindowToolbarWindowCollapseButton: '[&>svg]:size-4 text-amber-800',
+            uiWindowToolbarWindowCollapseButton:
+                '[&>svg]:size-4 text-amber-800',
             uiWindowToolbarWindowResizeHandle: 'bg-zinc-900',
             uiWindowToolbarWindowResizeHandleActive: 'bg-zinc-900',
         })
